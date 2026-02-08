198d24228de83a7afa31817f909f899543d8547a
DUWIPANGGA <aryad2232@gmail.com>
2026-02-09 01:31:40 +0700

    new version 2

--- env.c
+++ env.c
@@ -1,3 +1,3 @@
-#define VERSION_URL "https://raw.githubusercontent.com/Alvikh/TA-ARDUINO/master/controlling/version.md"
-#define FIRMWARE_URL "https://raw.githubusercontent.com/Alvikh/TA-ARDUINO/master/controlling/build/esp8266.esp8266.d1_mini/controlling.ino.bin"
-#define CURRENT_VERSION "1.0.2"
+#define VERSION_URL "https://raw.githubusercontent.com/DUWIPANGGA/starprint_rfid/master/version.md"
+#define FIRMWARE_URL "https://raw.githubusercontent.com/DUWIPANGGA/starprint_rfid/master/build/esp32.esp32.esp32/smarthome_starprint.ino.bin"
+#define CURRENT_VERSION "1.0.3"
